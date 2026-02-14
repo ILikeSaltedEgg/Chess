1d1bbce2dc8e74a1e762787f2cd2aa5432691006
Ace <155284890+ILikeSaltedEgg@users.noreply.github.com>
2026-02-14 21:40:50 +0800

    Refactor database connection code in config.php

--- api/config.php
+++ api/config.php
@@ -1,21 +1,18 @@
 <?php
-// Database configuration
+
 $host = 'localhost';
 $dbname = 'chess_game';
 $username = 'root';
 $password = '';
 
-// Start session
 session_start();
 
-// Create connection
 $conn = new mysqli($host, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Set charset
 $conn->set_charset("utf8mb4");
+
 ?>
