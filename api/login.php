20005bc27e8d2d45a05b3d35f66801a784e85492
Ace <155284890+ILikeSaltedEgg@users.noreply.github.com>
2026-02-19 23:41:06 +0800

    Update login.php

--- api/login.php
+++ api/login.php
@@ -5,8 +5,7 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
 require_once 'config.php';
-
-// Get POST data
+
 $data = json_decode(file_get_contents('php://input'), true);
 
 if (!$data) {
